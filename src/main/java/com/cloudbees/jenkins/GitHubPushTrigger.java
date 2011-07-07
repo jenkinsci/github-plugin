24fbe15119dbfa2f87df0f258ffbf7c15d267984
Jodok Batlogg <jodok@batlogg.com>
2011-07-07 22:20:05 +0200

    back to imports from eclipse

--- src/main/java/com/cloudbees/jenkins/GitHubPushTrigger.java
+++ src/main/java/com/cloudbees/jenkins/GitHubPushTrigger.java
@@ -19,8 +19,8 @@ import org.apache.commons.jelly.XMLOutput;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
-import org.spearce.jgit.transport.RemoteConfig;
-import org.spearce.jgit.transport.URIish;
+import org.eclipse.jgit.transport.RemoteConfig;
+import org.eclipse.jgit.transport.URIish;
 
 import java.io.File;
 import java.io.IOException;
