5b6d514e6dc81ae6bc201dab956c9f8645a06b6a
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-01-20 14:01:49 -0800

    we want nice transparency

--- src/main/java/com/cloudbees/jenkins/GitHubPushTrigger.java
+++ src/main/java/com/cloudbees/jenkins/GitHubPushTrigger.java
@@ -167,7 +167,7 @@ public class GitHubPushTrigger extends Trigger<AbstractProject> implements Runna
         }
 
         public String getIconFileName() {
-            return "clipboard.gif";
+            return "clipboard.png";
         }
 
         public String getDisplayName() {
