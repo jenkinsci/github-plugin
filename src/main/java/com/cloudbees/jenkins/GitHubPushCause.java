5331e1feaeb861f1564a773935f807eedeb95f3f
Eric Knecht <eric@ericknecht.com>
2012-04-17 14:41:08 -0400

    Change GitHubPushCause to extend from SCMTriggerCause

--- src/main/java/com/cloudbees/jenkins/GitHubPushCause.java
+++ src/main/java/com/cloudbees/jenkins/GitHubPushCause.java
@@ -1,13 +1,14 @@
 package com.cloudbees.jenkins;
 
 import hudson.model.Cause;
+import hudson.triggers.SCMTrigger.SCMTriggerCause;
 
 /**
  * UI object that says a build is started by GitHub post-commit hook.
  *
  * @author Kohsuke Kawaguchi
  */
-public class GitHubPushCause extends Cause {
+public class GitHubPushCause extends SCMTriggerCause {
     @Override
     public String getShortDescription() {
         return "Started by GitHub push by ";
