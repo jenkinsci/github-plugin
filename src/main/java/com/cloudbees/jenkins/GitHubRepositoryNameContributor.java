0f29b1d8d50003979dfb94f04c10a7196e831325
Nicolas De Loof <nicolas.deloof@gmail.com>
2014-09-04 14:09:33 +0200

    expand variables in git repository URL

--- src/main/java/com/cloudbees/jenkins/GitHubRepositoryNameContributor.java
+++ src/main/java/com/cloudbees/jenkins/GitHubRepositoryNameContributor.java
@@ -1,9 +1,12 @@
 package com.cloudbees.jenkins;
 
+import hudson.EnvVars;
 import hudson.Extension;
 import hudson.ExtensionList;
 import hudson.ExtensionPoint;
 import hudson.model.AbstractProject;
+import hudson.model.EnvironmentContributor;
+import hudson.model.TaskListener;
 import hudson.plugins.git.GitSCM;
 import hudson.scm.SCM;
 import jenkins.model.Jenkins;
@@ -11,6 +14,7 @@ import org.eclipse.jgit.transport.RemoteConfig;
 import org.eclipse.jgit.transport.URIish;
 import org.jenkinsci.plugins.multiplescms.MultiSCM;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
@@ -48,24 +52,34 @@ public abstract class GitHubRepositoryNameContributor implements ExtensionPoint 
     public static class FromSCM extends GitHubRepositoryNameContributor {
         @Override
         public void parseAssociatedNames(AbstractProject<?, ?> job, Collection<GitHubRepositoryName> result) {
+
+            EnvVars env = new EnvVars();
+            for (EnvironmentContributor contributor : EnvironmentContributor.all()) {
+                try {
+                    contributor.buildEnvironmentFor(job, env, TaskListener.NULL);
+                } catch (Exception e) {
+                    // ignore
+                }
+            }
+
             if (Jenkins.getInstance().getPlugin("multiple-scms") != null
                     && job.getScm() instanceof MultiSCM) {
                 MultiSCM multiSCM = (MultiSCM) job.getScm();
                 List<SCM> scmList = multiSCM.getConfiguredSCMs();
                 for (SCM scm : scmList) {
-                    addRepositories(scm, result);
+                    addRepositories(scm, env, result);
                 }
             } else {
-                addRepositories(job.getScm(), result);
+                addRepositories(job.getScm(), env, result);
             }
         }
 
-        private void addRepositories(SCM scm, Collection<GitHubRepositoryName> r) {
+        private void addRepositories(SCM scm, EnvVars env, Collection<GitHubRepositoryName> r) {
             if (scm instanceof GitSCM) {
                 GitSCM git = (GitSCM) scm;
                 for (RemoteConfig rc : git.getRepositories()) {
                     for (URIish uri : rc.getURIs()) {
-                        String url = uri.toString();
+                        String url = env.expand(uri.toString());
                         GitHubRepositoryName repo = GitHubRepositoryName.create(url);
                         if (repo != null) {
                             r.add(repo);
