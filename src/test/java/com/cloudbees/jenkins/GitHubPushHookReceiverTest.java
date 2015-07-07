57a5b4e477093c02bc79d71d1a5e3786c1c5aa70
Antonio Muñiz <amuniz@cloudbees.com>
2015-07-07 21:22:44 +0200

    [JENKINS-27136] Remove jgit dependency (git plugin decides what version is required) + fix test

--- src/test/java/com/cloudbees/jenkins/GitHubPushHookReceiverTest.java
+++ src/test/java/com/cloudbees/jenkins/GitHubPushHookReceiverTest.java
@@ -1,6 +1,10 @@
 package com.cloudbees.jenkins;
 
+import hudson.model.FreeStyleBuild;
+import hudson.model.Result;
 import hudson.model.FreeStyleProject;
+import hudson.model.Job;
+import hudson.model.Run;
 import hudson.plugins.git.SubmoduleConfig;
 import hudson.plugins.git.BranchSpec;
 import hudson.plugins.git.GitSCM;
@@ -13,6 +17,7 @@ import java.util.concurrent.ExecutionException;
 import javax.inject.Inject;
 
 import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
@@ -21,7 +26,7 @@ import org.jvnet.hudson.test.JenkinsRule;
  * Test case covering the code responsible to receive the push notification
  * from GitHub.
  */
-public class GitHubPushHookReceiverTest {
+public class GitHubPushHookReceiverTest extends Assert {
 
     @Rule
     public JenkinsRule j = new JenkinsRule();
@@ -39,10 +44,30 @@ public class GitHubPushHookReceiverTest {
             false, Collections.<SubmoduleConfig>emptyList(),
             null, null, null);
 
-        job.addTrigger(new GitHubPushTrigger());
+        GitHubPushTrigger trigger = new GitHubPushTrigger();
+        trigger.start(job, false);
+        job.addTrigger(trigger);
         job.setScm(scm);
 
-        String payload = IOUtils.toString(getClass().getResourceAsStream("payload2.json"));
+        String payload = IOUtils.toString(getClass().getResourceAsStream("/com/cloudbees/jenkins/plugins/github/payload2.json"));
         receiver.processGitHubPayload(payload, GitHubPushTrigger.class);
+        Run build = waitForBuild(1, job);
+        assertTrue("Build was triggered but did not success: " + build.getLog(300), Result.SUCCESS.equals(build.getResult()));
+    }
+
+    private Run waitForBuild(int n, Job job) throws InterruptedException {
+        System.out.println("Waiting for build #" + n + " to appear and finish");
+        int counter = 0;
+        while (true) {
+            if(counter > 30) {
+                assertTrue("Waiting more than a minute for the build to start", false);
+            }
+            Run b = job.getBuildByNumber(n);
+            if (b != null && !b.isBuilding()) {
+                return b;
+            }
+            Thread.sleep(2000);
+            counter++;
+        }
     }
 }
