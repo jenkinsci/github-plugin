c6609b8b04a241e7800f431c48d465e023047626
Kirill Merkushev <lanwen@yandex.ru>
2015-07-31 14:37:35 +0300

    [JENKINS-24702] add tests for new credentials usage code
    - for migration process
    - for changes in hooks
    - for configuration logic

---
+++ src/test/java/org/jenkinsci/plugins/github/config/GitHubServerConfigTest.java
@@ -0,0 +1,71 @@
+package org.jenkinsci.plugins.github.config;
+
+import org.junit.Test;
+
+import java.net.URI;
+
+import static org.hamcrest.Matchers.is;
+import static org.jenkinsci.plugins.github.config.GitHubServerConfig.GITHUB_URL;
+import static org.jenkinsci.plugins.github.config.GitHubServerConfig.allowedToManageHooks;
+import static org.jenkinsci.plugins.github.config.GitHubServerConfig.isUrlCustom;
+import static org.jenkinsci.plugins.github.config.GitHubServerConfig.withHost;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author lanwen (Merkushev Kirill)
+ */
+public class GitHubServerConfigTest {
+
+    public static final String CUSTOM_GH_SERVER = "http://some.com";
+    public static final String DEFAULT_GH_API_HOST = "api.github.com";
+
+    @Test
+    public void shouldMatchAllowedConfig() throws Exception {
+        assertThat(allowedToManageHooks().apply(new GitHubServerConfig("")), is(true));
+    }
+
+    @Test
+    public void shouldNotMatchNotAllowedConfig() throws Exception {
+        GitHubServerConfig input = new GitHubServerConfig("");
+        input.setDontUseItToMangeHooks(true);
+        assertThat(allowedToManageHooks().apply(input), is(false));
+    }
+
+    @Test
+    public void shouldMatchNonEqualToGHUrl() throws Exception {
+          assertThat(isUrlCustom(CUSTOM_GH_SERVER), is(true));
+    }
+
+    @Test
+    public void shouldNotMatchEmptyUrl() throws Exception {
+          assertThat(isUrlCustom(""), is(false));
+    }
+
+    @Test
+    public void shouldNotMatchNullUrl() throws Exception {
+          assertThat(isUrlCustom(null), is(false));
+    }
+
+    @Test
+    public void shouldNotMatchDefaultUrl() throws Exception {
+          assertThat(isUrlCustom(GITHUB_URL), is(false));
+    }
+
+    @Test
+    public void shouldMatchDefaultConfigWithGHDefaultHost() throws Exception {
+        assertThat(withHost(DEFAULT_GH_API_HOST).apply(new GitHubServerConfig("")), is(true));
+    }
+
+    @Test
+    public void shouldNotMatchNonDefaultConfigWithGHDefaultHost() throws Exception {
+        GitHubServerConfig input = new GitHubServerConfig("");
+        input.setCustom(true);
+        input.setApiUrl(CUSTOM_GH_SERVER);
+        assertThat(withHost(DEFAULT_GH_API_HOST).apply(input), is(false));
+    }
+
+    @Test
+    public void shouldNotMatchDefaultConfigWithNonDefaultHost() throws Exception {
+        assertThat(withHost(URI.create(CUSTOM_GH_SERVER).getHost()).apply(new GitHubServerConfig("")), is(false));
+    }
+}
---
+++ src/test/java/org/jenkinsci/plugins/github/migration/MigratorTest.java
@@ -0,0 +1,87 @@
+package org.jenkinsci.plugins.github.migration;
+
+import com.cloudbees.jenkins.GitHubPushTrigger;
+import com.cloudbees.jenkins.GitHubWebHook;
+import hudson.model.FreeStyleProject;
+import jenkins.model.Jenkins;
+import org.jenkinsci.plugins.github.GitHubPlugin;
+import org.jenkinsci.plugins.github.config.GitHubServerConfig;
+import org.jenkinsci.plugins.github.deprecated.Credential;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.recipes.LocalData;
+
+import java.io.IOException;
+
+import static java.lang.String.valueOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.both;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.jenkinsci.plugins.github.config.GitHubServerConfig.GITHUB_URL;
+import static org.jenkinsci.plugins.github.test.GitHubServerConfigMatcher.withApiUrl;
+import static org.jenkinsci.plugins.github.test.GitHubServerConfigMatcher.withCredsWithToken;
+
+/**
+ * @author lanwen (Merkushev Kirill)
+ */
+public class MigratorTest {
+
+    @Rule
+    public JenkinsRule jenkins = new JenkinsRule();
+
+    public static final String HOOK_FROM_LOCAL_DATA = "http://some.proxy.example.com/webhook";
+    public static final String CUSTOM_GH_URL = "http://custom.github.example.com/api/v3";
+    public static final String TOKEN = "some-oauth-token";
+    public static final String TOKEN2 = "some-oauth-token2";
+    public static final String TOKEN3 = "some-oauth-token3";
+
+    /**
+     * Just ignore mailformed hook in old config
+     */
+    @Test
+    @LocalData
+    public void shouldNotThrowExcMailformedHookUrlInOldConfig() throws IOException {
+        FreeStyleProject job = jenkins.createFreeStyleProject();
+        GitHubPushTrigger trigger = new GitHubPushTrigger();
+        trigger.start(job, true);
+        trigger.registerHooks();
+
+        assertThat("self hook url", trigger.getDescriptor().getDeprecatedHookUrl(), nullValue());
+        assertThat("imported hook url", valueOf(trigger.getDescriptor().getHookUrl()),
+                containsString(Jenkins.getInstance().getRootUrl() + GitHubWebHook.URLNAME));
+        assertThat("in plugin - override", GitHubPlugin.configuration().isOverrideHookURL(), is(false));
+    }
+
+    @Test
+    @LocalData
+    public void shouldMigrateHookUrl() {
+        assertThat("in plugin - override", GitHubPlugin.configuration().isOverrideHookURL(), is(true));
+        assertThat("in plugin", valueOf(GitHubPlugin.configuration().getHookUrl()), is(HOOK_FROM_LOCAL_DATA));
+
+        assertThat("should nullify hook url after migration",
+                GitHubPushTrigger.DescriptorImpl.get().getDeprecatedHookUrl(), nullValue());
+    }
+
+    @Test
+    @LocalData
+    public void shouldMigrateCredentials() throws Exception {
+        assertThat("should migrate 3 configs", GitHubPlugin.configuration().getConfigs(), hasSize(3));
+        assertThat("migrate custom url", GitHubPlugin.configuration().getConfigs(), hasItems(
+                both(withApiUrl(is(CUSTOM_GH_URL))).and(withCredsWithToken(TOKEN2)),
+                both(withApiUrl(is(GITHUB_URL))).and(withCredsWithToken(TOKEN)),
+                both(withApiUrl(is(GITHUB_URL))).and(withCredsWithToken(TOKEN3))
+        ));
+    }
+
+    @Test
+    public void shouldConvertCredsToServerConfig() throws Exception {
+        GitHubServerConfig conf = new Migrator().toGHServerConfig()
+                .apply(new Credential("name", CUSTOM_GH_URL, "token"));
+        assertThat(conf, both(withCredsWithToken("token")).and(withApiUrl(is(CUSTOM_GH_URL))));
+    }
+}
