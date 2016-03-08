d4a13b306ee0d73a00e16971c38dbaed6cb94fb4
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-03-07 20:28:30 -0800

    Sensible order is endpoint,credentials, then other stuff

--- src/main/resources/org/jenkinsci/plugins/github/config/GitHubServerConfig/config.groovy
+++ src/main/resources/org/jenkinsci/plugins/github/config/GitHubServerConfig/config.groovy
@@ -6,22 +6,12 @@ def f = namespace(lib.FormTagLib);
 def c = namespace(lib.CredentialsTagLib)
 
 
-f.entry(title: _("Manage hooks"), field: "manageHooks") {
-    f.checkbox(default: true)
-}
-
-f.entry(title: _("Credentials"), field: "credentialsId") {
-    c.select()
-}
-
 f.entry(title: _("API URL"), field: "apiUrl") {
     f.textbox(default: GitHubServerConfig.GITHUB_URL)
 }
 
-f.advanced() {
-    f.entry(title: _("GitHub client cache size (MB)"), field: "clientCacheSize") {
-        f.textbox(default: GitHubServerConfig.DEFAULT_CLIENT_CACHE_SIZE_MB)
-    }
+f.entry(title: _("Credentials"), field: "credentialsId") {
+    c.select()
 }
 
 f.block() {
@@ -33,3 +23,13 @@ f.block() {
     )
 }
 
+
+f.entry(title: _("Manage hooks"), field: "manageHooks") {
+    f.checkbox(default: true)
+}
+
+f.advanced() {
+    f.entry(title: _("GitHub client cache size (MB)"), field: "clientCacheSize") {
+        f.textbox(default: GitHubServerConfig.DEFAULT_CLIENT_CACHE_SIZE_MB)
+    }
+}
