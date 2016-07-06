1445eae331d787c64dfdfe7d8ecf1bc4d3f8f3d2
Mark Waite <mark.earl.waite@gmail.com>
2016-07-05 22:41:02 -0600

    Add annotator prototype test with Hamcrest assertions
    Attempt to keep the code DRY

---
+++ src/test/java/com/coravy/hudson/plugins/github/GitHubLinkAnnotatorNewTest.java
@@ -0,0 +1,90 @@
+package com.coravy.hudson.plugins.github;
+
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import hudson.MarkupText;
+import hudson.plugins.git.GitChangeSet;
+import java.util.ArrayList;
+import java.util.Random;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static java.lang.String.format;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(DataProviderRunner.class)
+public class GitHubLinkAnnotatorNewTest {
+
+    private final static String GITHUB_URL = "http://github.com/juretta/iphone-project-tools/";
+    private final static String SHA1 = "deadbeef36cd854f4dd6fa40bf94c0c657681dd5";
+    private final static Random RANDOM = new Random();
+    private final String expectedChangeSetAnnotation = " ("
+            + "<a href='" + GITHUB_URL + "commit/" + SHA1 + "'>"
+            + "commit: deadbee"
+            + "</a>)";
+    private static GitChangeSet changeSet;
+
+    @Before
+    public void createChangeSet() throws Exception {
+        ArrayList<String> lines = new ArrayList<String>();
+        lines.add("commit " + SHA1);
+        lines.add("tree 66236cf9a1ac0c589172b450ed01f019a5697c49");
+        lines.add("parent e74a24e995305bd67a180f0ebc57927e2b8783ce");
+        lines.add("author Author Name <author.name@nospam.com> 1363879004 +0100");
+        lines.add("committer Committer Name <committer.name@nospam.com> 1364199539 -0400");
+        lines.add("");
+        lines.add("    Committer and author are different in this commit.");
+        lines.add("");
+        changeSet = new GitChangeSet(lines, true);
+    }
+
+    private static Object[] genActualAndExpected(String keyword) {
+        int issueNumber = RANDOM.nextInt(1000000);
+        return new Object[]{
+            // Input text to the annotate method
+            "An issue " + keyword + " #" + issueNumber + " link",
+            // Output expected from the annotate method
+            "An issue "
+            + "<a href='" + GITHUB_URL + "issues/" + issueNumber + "/find'>"
+            + keyword + " #" + issueNumber
+            + "</a>"
+            + " link"};
+    }
+
+    @DataProvider
+    public static Object[][] annotations() {
+        return new Object[][]{
+            genActualAndExpected("Closes"),
+            genActualAndExpected("Close"),
+            genActualAndExpected("closes"),
+            genActualAndExpected("close")
+        };
+    }
+
+    @Test
+    @UseDataProvider("annotations")
+    public void inputIsExpected(String input, String expected) throws Exception {
+        assertThat(format("For input '%s'", input),
+                annotate(input, null),
+                is(expected));
+    }
+
+    @Test
+    @UseDataProvider("annotations")
+    public void inputIsExpectedWithChangeSet(String input, String expected) throws Exception {
+        assertThat(format("For changeset input '%s'", input),
+                annotate(input, changeSet),
+                is(expected + expectedChangeSetAnnotation));
+    }
+
+    private String annotate(final String originalText, GitChangeSet changeSet) {
+        MarkupText markupText = new MarkupText(originalText);
+
+        GithubLinkAnnotator annotator = new GithubLinkAnnotator();
+        annotator.annotate(new GithubUrl(GITHUB_URL), markupText, changeSet);
+
+        return markupText.toString(true);
+    }
+}
