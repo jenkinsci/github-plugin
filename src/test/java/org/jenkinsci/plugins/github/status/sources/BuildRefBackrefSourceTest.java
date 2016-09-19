fb3dd7050c324503b3e09e2dd9c8a4a2e85e3c65
Ivan Kalinin <pupssman@yandex-team.ru>
2016-09-20 00:01:48 +0300

    Change test to using JenkinsRule

--- src/test/java/org/jenkinsci/plugins/github/status/sources/BuildRefBackrefSourceTest.java
+++ src/test/java/org/jenkinsci/plugins/github/status/sources/BuildRefBackrefSourceTest.java
@@ -1,10 +1,12 @@
 package org.jenkinsci.plugins.github.status.sources;
 
+import hudson.model.FreeStyleProject;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.jvnet.hudson.test.JenkinsRule;
 import org.mockito.Answers;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
@@ -22,20 +24,22 @@ import static org.mockito.Mockito.when;
 public class BuildRefBackrefSourceTest {
 
     @Rule
-    public MockitoRule mockitoRule = MockitoJUnit.rule();
-
-    @Mock(answer = Answers.RETURNS_MOCKS)
-    private Run run;
+    public JenkinsRule jenkinsRule = new JenkinsRule();
 
     @Mock(answer = Answers.RETURNS_MOCKS)
     private TaskListener listener;
 
     @Test
+    /**
+     * Should've used mocked Run, but getAbsoluteUrl is final.
+     *
+     * @throws Exception
+     */
     public void shouldReturnRunAbsoluteUrl() throws Exception {
-        when(run.getAbsoluteUrl()).thenReturn("ABSOLUTE_URL");
+        Run<?, ?> run = jenkinsRule.buildAndAssertSuccess(jenkinsRule.createFreeStyleProject());
 
         String result = new BuildRefBackrefSource().get(run, listener);
-        assertThat("state", result, is("ABSOLUTE_URL"));
+        assertThat("state", result, is(run.getAbsoluteUrl()));
     }
 
 }
