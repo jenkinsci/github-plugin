f4cf8a3d138310fe232993e5cbd6a848b1fbb97f
Kirill Merkushev <lanwen@yandex.ru>
2016-10-03 19:22:59 +0300

    [FIXES JENKINS-38665] Add databound constructor for default backref source
    https://issues.jenkins-ci.org/browse/JENKINS-38665

--- src/main/java/org/jenkinsci/plugins/github/status/sources/BuildRefBackrefSource.java
+++ src/main/java/org/jenkinsci/plugins/github/status/sources/BuildRefBackrefSource.java
@@ -1,20 +1,23 @@
 package org.jenkinsci.plugins.github.status.sources;
 
-import org.jenkinsci.plugins.github.extension.status.GitHubStatusBackrefSource;
-
 import hudson.Extension;
 import hudson.model.Descriptor;
 import hudson.model.Run;
 import hudson.model.TaskListener;
+import org.jenkinsci.plugins.github.extension.status.GitHubStatusBackrefSource;
+import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
  * Gets backref from Run URL.
  *
  * @author pupssman (Kalinin Ivan)
- * @since 1.21.2
- *
+ * @since 1.22.1
  */
 public class BuildRefBackrefSource extends GitHubStatusBackrefSource {
+
+    @DataBoundConstructor
+    public BuildRefBackrefSource() {
+    }
 
     /**
      * Returns absolute URL of the Run
