f784e19fce776a0290ebe5ea94a3233574112124
James William Dumay <jdumay@cloudbees.com>
2016-11-16 20:37:47 +1100

    GitHubWebHookCrumbExclusion should be more forgiving if the user leaves off the trailing slash (#152)

--- src/main/java/com/cloudbees/jenkins/GitHubWebHookCrumbExclusion.java
+++ src/main/java/com/cloudbees/jenkins/GitHubWebHookCrumbExclusion.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
 @Extension
 public class GitHubWebHookCrumbExclusion extends CrumbExclusion {
 
@@ -16,11 +18,16 @@ public class GitHubWebHookCrumbExclusion extends CrumbExclusion {
     public boolean process(HttpServletRequest req, HttpServletResponse resp, FilterChain chain)
             throws IOException, ServletException {
         String pathInfo = req.getPathInfo();
-        if (pathInfo != null && pathInfo.equals(getExclusionPath())) {
-            chain.doFilter(req, resp);
-            return true;
+        if (isEmpty(pathInfo)) {
+            return false;
         }
-        return false;
+        // Github will not follow redirects https://github.com/isaacs/github/issues/574
+        pathInfo = pathInfo.endsWith("/") ? pathInfo : pathInfo + '/';
+        if (!pathInfo.equals(getExclusionPath())) {
+            return false;
+        }
+        chain.doFilter(req, resp);
+        return true;
     }
 
     public String getExclusionPath() {
