71ae920824983a18e24f849d342fd293ae65ee53
Raul <rarabaolaza@cloudbees.com>
2016-11-24 13:11:00 +0100

    [JENKINS-39822] Invoke build getting into account git plugin version
    * Git plugin 2.4.1+ does not include BuildData if checkout fails,
    resulting in testNoBuildRevision failing

--- src/test/java/com/cloudbees/jenkins/GitHubCommitNotifierTest.java
+++ src/test/java/com/cloudbees/jenkins/GitHubCommitNotifierTest.java
@@ -99,11 +99,7 @@ public class GitHubCommitNotifierTest {
         prj.setScm(new GitSCM("http://non.existent.git.repo.nowhere/repo.git"));
         prj.getPublishersList().add(new GitHubCommitNotifier());
         Build b = null;
-        if (jRule.getPluginManager().getPlugin("git").getVersionNumber().isNewerThan(new VersionNumber("2.4.0"))) {
-            b = prj.scheduleBuild2(0, new Cause.UserIdCause(), new BuildData()).get();
-        } else {
-            b = prj.scheduleBuild2(0).get();
-        }
+        b = safelyGenerateBuild(prj);
         jRule.assertBuildStatus(Result.FAILURE, b);
         jRule.assertLogContains(BuildDataHelper_NoLastRevisionError(), b);
     }
@@ -144,6 +140,17 @@ public class GitHubCommitNotifierTest {
         prj.scheduleBuild2(0).get();
 
         github.service().verify(1, postRequestedFor(urlPathMatching(".*/" + SOME_SHA)));
+    }
+
+    private Build safelyGenerateBuild(FreeStyleProject prj) throws InterruptedException, java.util.concurrent.ExecutionException {
+        //Git plugin 2.4.1 + does not include BuildData if checkout fails, so we add it if needed
+        Build b;
+        if (jRule.getPluginManager().getPlugin("git").getVersionNumber().isNewerThan(new VersionNumber("2.4.0"))) {
+            b = prj.scheduleBuild2(0, new Cause.UserIdCause(), new BuildData()).get();
+        } else {
+            b = prj.scheduleBuild2(0).get();
+        }
+        return b;
     }
 
     @TestExtension
