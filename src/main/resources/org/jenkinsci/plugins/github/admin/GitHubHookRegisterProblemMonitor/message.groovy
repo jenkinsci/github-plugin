22bb3fb49f6e18730da2e73d3ec4c86c4ee20dd4
Manuel Recena <recena@gmail.com>
2017-10-29 19:32:00 +0100

    [JENKINS-43786] More compatible

--- src/main/resources/org/jenkinsci/plugins/github/admin/GitHubHookRegisterProblemMonitor/message.groovy
+++ src/main/resources/org/jenkinsci/plugins/github/admin/GitHubHookRegisterProblemMonitor/message.groovy
@@ -1,11 +1,24 @@
 package org.jenkinsci.plugins.github.admin.GitHubHookRegisterProblemMonitor
 
+import hudson.util.VersionNumber
+import jenkins.model.Jenkins
+
 def f = namespace(lib.FormTagLib)
 
-div(class: 'alert alert-warning') {
-    form(method: 'post', action: "${rootURL}/${my?.url}/act", name: my?.id) {
-        f.submit(name: 'yes', value: _('view'))
-        f.submit(name: 'no', value: _('dismiss'))
+if (Jenkins.getVersion().isNewerThan(new VersionNumber("2.88"))) {
+    div(class: 'alert alert-warning') {
+        form(method: 'post', action: "${rootURL}/${my?.url}/act", name: my?.id) {
+            f.submit(name: 'yes', value: _('view'))
+            f.submit(name: 'no', value: _('dismiss'))
+        }
+        text(_('hook.registering.problem'))
     }
-    text(_('hook.registering.problem'))
+} else {
+    div(class: 'warning') {
+        form(method: 'post', action: "${rootURL}/${my?.url}/act", name: my?.id) {
+            text(_('hook.registering.problem'))
+            f.submit(name: 'yes', value: _('view'))
+            f.submit(name: 'no', value: _('dismiss'))
+        }
+    }
 }
