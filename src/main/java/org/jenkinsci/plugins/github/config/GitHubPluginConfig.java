9389d8bd8e63755a9568a7dc6edcb21fac72c767
Daniel Garzon <d3@digitalasset.com>
2019-02-01 15:12:23 -0500

    JENKINS-55920: address PR comments.

--- src/main/java/org/jenkinsci/plugins/github/config/GitHubPluginConfig.java
+++ src/main/java/org/jenkinsci/plugins/github/config/GitHubPluginConfig.java
@@ -102,12 +102,17 @@ public class GitHubPluginConfig extends GlobalConfiguration {
     }
 
     @DataBoundSetter
-    public void setHookUrl(String hookUrl) {
+    public void setHookUrl(URL hookUrl) {
         if (overrideHookUrl) {
-            this.hookUrl = parseHookUrl(hookUrl);
+            this.hookUrl = hookUrl;
         } else {
             this.hookUrl = null;
         }
+    }
+
+    @SuppressWarnings("unused")
+    public boolean isOverrideHookUrl() {
+        return overrideHookUrl;
     }
 
     @DataBoundSetter
@@ -267,13 +272,5 @@ public class GitHubPluginConfig extends GlobalConfiguration {
     @DataBoundSetter
     public void setHookSecretConfig(HookSecretConfig hookSecretConfig) {
         this.hookSecretConfig = hookSecretConfig;
-    }
-
-    private URL parseHookUrl(String hookUrl) {
-        try {
-            return new URL(hookUrl);
-        } catch (MalformedURLException e) {
-            return null;
-        }
     }
 }
