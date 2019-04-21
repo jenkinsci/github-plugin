4c98e979077c7a7ea915244de35d52bd20e26278
Eric Peters <eric@peters.org>
2019-04-21 11:44:45 -0700

    Add org.jenkinsci.plugins.github.config.ConfigAsCodeTest for JENKINS-55920

--- src/test/java/org/jenkinsci/plugins/github/test/GitHubServerConfigMatcher.java
+++ src/test/java/org/jenkinsci/plugins/github/test/GitHubServerConfigMatcher.java
@@ -23,11 +23,47 @@ public final class GitHubServerConfigMatcher {
         };
     }
 
+    public static Matcher<GitHubServerConfig> withClientCacheSize(Matcher<Integer> matcher) {
+        return new FeatureMatcher<GitHubServerConfig, Integer>(matcher, "client cache size", "") {
+            @Override
+            protected Integer featureValueOf(GitHubServerConfig actual) {
+                return actual.getClientCacheSize();
+            }
+        };
+    }
+
+    public static Matcher<GitHubServerConfig> withCredsId(Matcher<String> matcher) {
+        return new FeatureMatcher<GitHubServerConfig, String>(matcher, "credentials id", "") {
+            @Override
+            protected String featureValueOf(GitHubServerConfig actual) {
+                return actual.getCredentialsId();
+            }
+        };
+    }
+
     public static Matcher<GitHubServerConfig> withCredsWithToken(String token) {
         return new FeatureMatcher<GitHubServerConfig, String>(is(token), "token in creds", "") {
             @Override
             protected String featureValueOf(GitHubServerConfig actual) {
                 return tokenFor(actual.getCredentialsId());
+            }
+        };
+    }
+
+    public static Matcher<GitHubServerConfig> withIsManageHooks(Matcher<Boolean> matcher) {
+        return new FeatureMatcher<GitHubServerConfig, Boolean>(matcher, "is manage hooks", "") {
+            @Override
+            protected Boolean featureValueOf(GitHubServerConfig actual) {
+                return actual.isManageHooks();
+            }
+        };
+    }
+
+    public static Matcher<GitHubServerConfig> withName(Matcher<String> matcher) {
+        return new FeatureMatcher<GitHubServerConfig, String>(matcher, "name", "") {
+            @Override
+            protected String featureValueOf(GitHubServerConfig actual) {
+                return actual.getName();
             }
         };
     }
