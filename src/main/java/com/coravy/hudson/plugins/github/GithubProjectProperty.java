f986e40b777e05273a177a615ba4cfab6a23a83c
FHLBI-EC\iapughg <gpugh@fhlbi.com>
2020-11-19 11:46:54 -0500

    JENKINS-62339 - Fix checkstyle violation (LineLength: Line is longer than 120 characters)

--- src/main/java/com/coravy/hudson/plugins/github/GithubProjectProperty.java
+++ src/main/java/com/coravy/hudson/plugins/github/GithubProjectProperty.java
@@ -116,7 +116,9 @@ public final class GithubProjectProperty extends JobProperty<Job<?, ?>> {
         }
 
         @Override
-        public JobProperty<?> newInstance(@Nonnull StaplerRequest req, JSONObject formData) throws Descriptor.FormException {
+        public JobProperty<?> newInstance(@Nonnull StaplerRequest req,
+		                                  JSONObject formData) throws Descriptor.FormException {
+
             GithubProjectProperty tpp = req.bindJSON(
                     GithubProjectProperty.class,
                     formData.getJSONObject(GITHUB_PROJECT_BLOCK_NAME)
