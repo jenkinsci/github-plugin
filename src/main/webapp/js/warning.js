9afbdf9c09c74db4dec06ed6b4785201cb105ef7
krystaltt <naichuat@uci.edu>
2024-07-24 20:52:48 -0700

    Un-inlining JS in GitHubPushTrigger/config.groovy

--- src/main/webapp/js/warning.js
+++ src/main/webapp/js/warning.js
@@ -9,6 +9,16 @@ var InlineWarning = (function () {
 
     exports.setup = function (opts) {
         options = opts;
+
+        // Check if the URL needs concatenation
+        if (opts.url.includes("'+'")) {
+            // Manually concatenate the parts
+            let parts = opts.url.split("'+'");
+            options.url = parts.map(part => part.replace(/'/g, '')).join('');
+        } else {
+            options.url = opts.url;
+        }
+
         return exports;
     };
 
@@ -39,3 +49,24 @@ var InlineWarning = (function () {
 
     return exports;
 })();
+
+document.addEventListener('DOMContentLoaded', function() {
+    var warningElement = document.getElementById('gh-hooks-warn');
+
+    if (warningElement) {
+        var url = warningElement.getAttribute('data-url');
+        var input = warningElement.getAttribute('data-input');
+
+        if (url && input) {
+            InlineWarning.setup({
+                id: 'gh-hooks-warn',
+                url: url,
+                input: input
+            }).start();
+        } else {
+            console.error('URL or Input is null');
+        }
+    } else {
+        console.error('Element with ID "gh-hooks-warn" not found');
+    }
+});
